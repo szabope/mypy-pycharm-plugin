604c19d11a50a9e6e6b88463b4fbb712e01a4201
Sean Flanigan <sean@flanigan.org>
2025-01-26 22:02:43 +1000

    Accommodate spaces when executing commands

--- src/main/kotlin/works/szabope/plugins/mypy/configurable/MypySettingConfigurable.kt
+++ src/main/kotlin/works/szabope/plugins/mypy/configurable/MypySettingConfigurable.kt
@@ -86,7 +86,7 @@ internal class MypySettingConfigurable(private val project: Project) : BoundSear
                     }).align(AlignX.RIGHT + AlignY.CENTER)
                 }.rowComment(
                     MyBundle.message(
-                        "mypy.settings.path_to_executable.comment", MypyArgs.MYPY_MANDATORY_COMMAND_ARGS
+                        "mypy.settings.path_to_executable.comment", MypyArgs.MYPY_MANDATORY_COMMAND_ARGS.joinToString(" ")
                     ), maxLineLength = MAX_LINE_LENGTH_WORD_WRAP
                 ).layout(RowLayout.PARENT_GRID)
                 row {
--- src/main/kotlin/works/szabope/plugins/mypy/services/MypySettings.kt
+++ src/main/kotlin/works/szabope/plugins/mypy/services/MypySettings.kt
@@ -159,7 +159,7 @@ class MypySettings(internal val project: Project) :
 
             val stdout = StringBuilder()
             val processResult = runBlocking {
-                Cli().execute("$path -V") { it.collect(stdout::appendLine) }
+                Cli().execute(listOf("$path", "-V")) { it.collect(stdout::appendLine) }
             }
             if (processResult.resultCode != 0) {
                 return SettingsValidationProblem(
@@ -217,7 +217,7 @@ class MypySettings(internal val project: Project) :
     }
 
     suspend fun autodetectExecutable(): String? {
-        val locateCommand = if (SystemInfo.isWindows) "where.exe mypy.exe" else "which mypy"
+        val locateCommand = if (SystemInfo.isWindows) listOf("where.exe", "mypy.exe") else listOf("which", "mypy")
         val stdout = StringBuilder()
         val processResult = PythonEnvironmentAwareCli(project).execute(locateCommand) { it.collect(stdout::appendLine) }
         return when (processResult.resultCode) { // same for linux and windows
@@ -229,7 +229,7 @@ class MypySettings(internal val project: Project) :
                 ) {
                     if (it.eventType == HyperlinkEvent.EventType.ACTIVATED) {
                         IDialogManager.showMypyExecutionErrorDialog(
-                            locateCommand,
+                            locateCommand.joinToString(" "),
                             processResult.stderr,
                             processResult.resultCode
                         )
