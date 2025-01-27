cd4df1f3c0bf51c6a462e2e01cbfd76bff5a9712
szabope <szabope@users.noreply.github.com>
2025-01-27 10:54:22 +0100

    add test for space in mypy path (#49)

--- src/test/kotlin/works/szabope/plugins/mypy/annotator/MypyAnnotatorTest.kt
+++ src/test/kotlin/works/szabope/plugins/mypy/annotator/MypyAnnotatorTest.kt
@@ -4,6 +4,9 @@ import com.intellij.testFramework.TestDataPath
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
 import works.szabope.plugins.mypy.MyBundle
 import works.szabope.plugins.mypy.services.MypySettings
+import java.nio.file.Paths
+import kotlin.io.path.absolutePathString
+import kotlin.io.path.pathString
 
 @TestDataPath("\$CONTENT_ROOT/testData/annotation")
 class MypyAnnotatorTest : BasePlatformTestCase() {
@@ -22,5 +25,18 @@ class MypyAnnotatorTest : BasePlatformTestCase() {
                                 |""".trimMargin()
         )
         assertEmpty(myFixture.filterAvailableIntentions(MyBundle.message("mypy.intention.ignore.text")))
+    }
+
+    fun `test MypyAnnotator does not fail if mypy executable path has a space in it`() {
+        with(MypySettings.getInstance(project)) {
+            mypyExecutable = Paths.get(testDataPath).resolve("white space/mypy").absolutePathString()
+            projectDirectory = Paths.get(testDataPath).pathString
+        }
+        myFixture.configureByText(
+            "a.py", """|def<caret> lets_have_fun() -> [int]:
+                                |   return 'fun'
+                                |""".trimMargin()
+        )
+        assertNotEmpty(myFixture.filterAvailableIntentions(MyBundle.message("mypy.intention.ignore.text")))
     }
 }
