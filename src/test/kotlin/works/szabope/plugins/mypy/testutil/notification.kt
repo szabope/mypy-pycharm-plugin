e334dcd85f29f8dc7c335fd82fe4fbcc2cf6ab89
Peter Szabo <szabope@users.noreply.github.com>
2025-09-20 13:30:47 +0000

    test initialization with local sdk

--- src/test/kotlin/works/szabope/plugins/mypy/testutil/notification.kt
+++ src/test/kotlin/works/szabope/plugins/mypy/testutil/notification.kt
@@ -5,7 +5,7 @@ import com.intellij.notification.Notification
 import com.intellij.openapi.project.Project
 import works.szabope.plugins.mypy.MypyBundle
 
-context(project: Project) fun getPylintConfigurationNotCompleteNotification(): Notification {
+context(project: Project) fun getMypyConfigurationNotCompleteNotification(): Notification {
     return ActionCenter.getNotifications(project).single {
         MypyBundle.message("mypy.notification.group") == it.groupId && MypyBundle.message("mypy.notification.incomplete_configuration") == it.content && !it.isExpired
     }
