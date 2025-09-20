19eb60c4a34e96549bdf1cdb38fd69a5b60470f5
Peter Szabo <szabope@users.noreply.github.com>
2025-09-20 13:34:07 +0000

    test initialization without sdk

--- src/test/kotlin/works/szabope/plugins/mypy/PluginInitializationFromScratchTest.kt
+++ src/test/kotlin/works/szabope/plugins/mypy/PluginInitializationFromScratchTest.kt
@@ -1,11 +1,11 @@
 package works.szabope.plugins.mypy
 
-import works.szabope.plugins.mypy.testutil.getPylintConfigurationNotCompleteNotification
+import works.szabope.plugins.mypy.testutil.getMypyConfigurationNotCompleteNotification
 
 class PluginInitializationFromScratchTest : AbstractToolWindowTestCase() {
 
     fun `test plugin initialized from scratch (no python sdk) results in notification`() {
-        val actions = with(project) { getPylintConfigurationNotCompleteNotification() }.actions
+        val actions = with(project) { getMypyConfigurationNotCompleteNotification() }.actions
         assertEquals(
             MypyBundle.message("mypy.intention.complete_configuration.text"),
             actions.single().templatePresentation.text
