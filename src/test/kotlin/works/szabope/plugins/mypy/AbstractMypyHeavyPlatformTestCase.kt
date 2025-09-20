bf3a3f989d62b881119b424bcb23046f1d1d00a0
Peter Szabo <szabope@users.noreply.github.com>
2025-09-20 13:27:39 +0000

    test initialization from old configuration

---
+++ src/test/kotlin/works/szabope/plugins/mypy/AbstractMypyHeavyPlatformTestCase.kt
@@ -0,0 +1,30 @@
+package works.szabope.plugins.mypy
+
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.newvfs.impl.VfsRootAccess
+import com.intellij.testFramework.HeavyPlatformTestCase
+import io.mockk.every
+import io.mockk.mockkObject
+import works.szabope.plugins.common.services.PluginPackageManagementService
+import works.szabope.plugins.mypy.services.MypyPluginPackageManagementService
+import works.szabope.plugins.mypy.testutil.MypyPluginPackageManagementServiceStub
+
+abstract class AbstractMypyHeavyPlatformTestCase : HeavyPlatformTestCase() {
+
+    // local variables are not supported in mockk answer, yet
+    private lateinit var mypyPackageManagementServiceStub: PluginPackageManagementService
+
+    override fun setUp() {
+        VfsRootAccess.allowRootAccess(testRootDisposable, "/usr/bin")
+        mockkObject(MypyPluginPackageManagementService.Companion)
+        every { MypyPluginPackageManagementService.getInstance(any(Project::class)) } answers {
+            if (!::mypyPackageManagementServiceStub.isInitialized) {
+                mypyPackageManagementServiceStub = MypyPluginPackageManagementServiceStub(
+                    firstArg<Project>()
+                )
+            }
+            mypyPackageManagementServiceStub
+        }
+        super.setUp()
+    }
+}
---
+++ src/test/kotlin/works/szabope/plugins/mypy/MypyInitializationFromOldConfigurationTest.kt
@@ -0,0 +1,28 @@
+package works.szabope.plugins.mypy
+
+import com.intellij.openapi.vfs.newvfs.impl.VfsRootAccess
+import com.intellij.testFramework.PlatformTestUtil
+import works.szabope.plugins.mypy.services.MypySettings
+import java.nio.file.Path
+
+class MypyInitializationFromOldConfigurationTest : AbstractMypyHeavyPlatformTestCase() {
+
+    override fun setUpProject() {
+        VfsRootAccess.allowRootAccess(testRootDisposable, "/usr/bin")
+        myProject = PlatformTestUtil.loadAndOpenProject(Path.of(PROJECT_PATH), getTestRootDisposable())
+    }
+
+    fun `test plugin initialized from old configuration`() {
+        with(MypySettings.getInstance(project)) {
+            assertFalse(useProjectSdk)
+            assertEquals("$PROJECT_PATH/.venv/bin/mypy", executablePath)
+            assertEquals("--show-column-numbers", arguments)
+            assertFalse(scanBeforeCheckIn)
+            assertEquals("$PROJECT_PATH/mypy.conf", configFilePath) //
+        }
+    }
+
+    companion object {
+        const val PROJECT_PATH = "src/test/testData/initialization/OldConfiguration"
+    }
+}
