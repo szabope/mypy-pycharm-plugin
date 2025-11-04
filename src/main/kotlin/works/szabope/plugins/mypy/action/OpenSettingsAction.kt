dc928a1bb12a46c4e5c05ac9396601b69983c460
Peter Szabo <szabope@users.noreply.github.com>
2025-11-04 12:31:03 +0000

    - configure name for action in notification - remove unused code

--- src/main/kotlin/works/szabope/plugins/mypy/action/OpenSettingsAction.kt
+++ src/main/kotlin/works/szabope/plugins/mypy/action/OpenSettingsAction.kt
@@ -6,9 +6,10 @@ import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.options.ShowSettingsUtil
 import com.intellij.openapi.wm.ToolWindowManager
+import works.szabope.plugins.mypy.MypyBundle
 import works.szabope.plugins.mypy.configurable.MypyConfigurable
 
-class OpenSettingsAction : AnAction() {
+class OpenSettingsAction : AnAction(MypyBundle.message("mypy.intention.complete_configuration.text")) {
     override fun actionPerformed(e: AnActionEvent) {
         val project = e.project ?: return
         ToolWindowManager.getInstance(project).invokeLater {
