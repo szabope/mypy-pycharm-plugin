c217a6d201fc10294e8e67455610053b5b5789e8
Peter Szabo <szabope@users.noreply.github.com>
2025-11-20 15:00:17 +0000

    do not run annotator for virtual files (e.g. python console)

--- src/main/kotlin/works/szabope/plugins/mypy/annotator/MypyAnnotator.kt
+++ src/main/kotlin/works/szabope/plugins/mypy/annotator/MypyAnnotator.kt
@@ -12,6 +12,7 @@ import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.profile.codeInspection.InspectionProjectProfileManager
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
+import com.intellij.testFramework.LightVirtualFile
 import com.intellij.util.DocumentUtil
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOf
@@ -26,7 +27,9 @@ class MypyAnnotator : ExternalAnnotator<MypyAnnotator.AnnotatorInfo, Flow<MypyMe
 
     class AnnotatorInfo(val file: VirtualFile, val project: Project)
 
-    override fun collectInformation(file: PsiFile): AnnotatorInfo {
+    override fun collectInformation(file: PsiFile): AnnotatorInfo? {
+        // do not run for in-memory files, shadowing would fail
+        if (file.virtualFile is LightVirtualFile) return null
         return AnnotatorInfo(file.virtualFile, file.project)
     }
 
--- src/test/kotlin/works/szabope/plugins/mypy/annotator/AnnotatorTest.kt
+++ src/test/kotlin/works/szabope/plugins/mypy/annotator/AnnotatorTest.kt
@@ -1,13 +1,15 @@
 package works.szabope.plugins.mypy.annotator
 
+import com.intellij.testFramework.LightVirtualFile
 import com.intellij.testFramework.TestDataPath
-import works.szabope.plugins.mypy.AbstractMypyTestCase
+import junit.framework.AssertionFailedError
+import works.szabope.plugins.mypy.AbstractToolWindowTestCase
 import works.szabope.plugins.mypy.services.MypySettings
 import java.nio.file.Paths
 import kotlin.io.path.absolutePathString
 
 @TestDataPath($$"$CONTENT_ROOT/testData/annotation")
-class AnnotatorTest : AbstractMypyTestCase() {
+class AnnotatorTest : AbstractToolWindowTestCase() {
     companion object {
         val DOESNT_MATTER = """|def<caret> lets_have_fun() -> [int]:
                                 |   return 'fun'
@@ -42,5 +44,24 @@ class AnnotatorTest : AbstractMypyTestCase() {
         @Suppress("UnstableApiUsage") val mypyAnnotations =
             myFixture.doHighlighting().filter { it.toolId == MypyAnnotator::class.java }
         assertEquals(1, mypyAnnotations.size)
+    }
+
+    fun `test MypyAnnotator does not run for in-memory target`() {
+        with(MypySettings.getInstance(project)) {
+            executablePath = Paths.get(testDataPath).resolve("does_not_exist").absolutePathString()
+            workingDirectory = Paths.get(testDataPath).absolutePathString()
+            arguments = ""
+            useProjectSdk = false
+        }
+        var assertionError: Error? = null
+        toolWindowManager.onBalloon {
+            assertionError = AssertionFailedError("Should not happen: $it")
+        }
+        val inMemoryTarget = LightVirtualFile("file-in-memory.py", "")
+        myFixture.configureFromExistingVirtualFile(inMemoryTarget)
+        @Suppress("UnstableApiUsage") val mypyAnnotations =
+            myFixture.doHighlighting().filter { it.toolId == MypyAnnotator::class.java }
+        assertEquals(0, mypyAnnotations.size)
+        assertionError?.let { throw it }
     }
 }
