b43f099615ce609d7d2520b598ff80e9320722d2
Peter Szabo <szabope@users.noreply.github.com>
2025-12-06 15:53:15 +0000

    apply general solutions

--- src/main/kotlin/works/szabope/plugins/mypy/services/parser/MypyMessage.kt
+++ src/main/kotlin/works/szabope/plugins/mypy/services/parser/MypyMessage.kt
@@ -1,16 +1,17 @@
 package works.szabope.plugins.mypy.services.parser
 
 import kotlinx.serialization.Serializable
+import works.szabope.plugins.common.annotator.ToolMessage
 
 // https://github.com/Kotlin/kotlinx.serialization/issues/2808
 @Suppress("PROVIDED_RUNTIME_TOO_LOW", "INLINE_CLASSES_NOT_SUPPORTED")
 @Serializable
 data class MypyMessage(
     val file: String,
-    var line: Int,
-    val column: Int,
-    val message: String,
+    override var line: Int,
+    override val column: Int,
+    override val message: String,
     val hint: String?,
     val code: String,
     var severity: String
-)
+) : ToolMessage
