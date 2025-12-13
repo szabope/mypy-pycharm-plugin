a43791c5db06bc87e8e6b79c5efbcddc48f98d9a
szabope <szabope@users.noreply.github.com>
2025-12-13 03:52:42 +0000

    Handle mypy exit 2 (#148)
    * handle mypy exit 2
    
    * fix dialog panel size

--- src/main/kotlin/works/szabope/plugins/mypy/services/MypyExecutor.kt
+++ src/main/kotlin/works/szabope/plugins/mypy/services/MypyExecutor.kt
@@ -7,6 +7,6 @@ import works.szabope.plugins.common.run.ToolExecutor
 class MypyExecutor(project: Project) : ToolExecutor(project, "mypy") {
     override fun isError(event: ProcessEvent): Boolean {
         // exit code 1 should be fine https://github.com/python/mypy/issues/6003
-        return event.exitCode > 1
+        return event.exitCode > 2
     }
 }
