58828cac64fbd557518da38aa341a3db2bbebb99
Peter Szabo <szabope@users.noreply.github.com>
2026-03-26 17:39:54 +0100

    refactor tool validation and its messages

--- src/main/kotlin/works/szabope/plugins/mypy/configurable/MypyValidator.kt
+++ src/main/kotlin/works/szabope/plugins/mypy/configurable/MypyValidator.kt
@@ -2,17 +2,23 @@ package works.szabope.plugins.mypy.configurable
 
 import com.intellij.openapi.project.Project
 import works.szabope.plugins.common.validator.AbstractToolValidator
+import works.szabope.plugins.common.validator.ToolValidatorMessages
 import works.szabope.plugins.mypy.MypyBundle
 import works.szabope.plugins.mypy.services.MypyPluginPackageManagementService
 
-class MypyValidator(project: Project) : AbstractToolValidator(project) {
+class MypyValidator(project: Project) : AbstractToolValidator(project, MESSAGES) {
     override val versionFlag = "-V"
     override val packageName = "mypy"
     override fun getPackageManagementService() = MypyPluginPackageManagementService.getInstance(project)
-    override fun pathNotExistsMessage() = MypyBundle.message("mypy.configuration.path_to_executable.not_exists")
-    override fun pathIsDirectoryMessage() = MypyBundle.message("mypy.configuration.path_to_executable.is_directory")
-    override fun pathNotExecutableMessage() = MypyBundle.message("mypy.configuration.path_to_executable.not_executable")
-    override fun unknownVersionMessage() = MypyBundle.message("mypy.configuration.path_to_executable.unknown_version")
-    override fun invalidVersionMessage() = MypyBundle.message("mypy.configuration.mypy_invalid_version")
-    override fun notInstalledMessage() = MypyBundle.message("mypy.configuration.mypy_not_installed")
+
+    companion object {
+        private val MESSAGES = ToolValidatorMessages(
+            pathNotExists = MypyBundle.message("mypy.configuration.path_to_executable.not_exists"),
+            pathIsDirectory = MypyBundle.message("mypy.configuration.path_to_executable.is_directory"),
+            pathNotExecutable = MypyBundle.message("mypy.configuration.path_to_executable.not_executable"),
+            unknownVersion = MypyBundle.message("mypy.configuration.path_to_executable.unknown_version"),
+            invalidVersion = MypyBundle.message("mypy.configuration.mypy_invalid_version"),
+            notInstalled = MypyBundle.message("mypy.configuration.mypy_not_installed")
+        )
+    }
 }
