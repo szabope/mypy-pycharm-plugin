5d2e8ae4540d5d4f9dcd9e19476e19dbf0865c11
Peter Szabo <szabope@users.noreply.github.com>
2026-03-26 11:17:00 +0100

    fix internal api use

--- src/main/kotlin/works/szabope/plugins/mypy/services/mypyParamListBuilders.kt
+++ src/main/kotlin/works/szabope/plugins/mypy/services/mypyParamListBuilders.kt
@@ -3,9 +3,10 @@ package works.szabope.plugins.mypy.services
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.io.toCanonicalPath
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.platform.backend.workspace.WorkspaceModel
 import com.intellij.platform.backend.workspace.virtualFile
+import com.intellij.platform.workspace.jps.entities.ContentRootEntity
 import com.intellij.platform.workspace.jps.entities.ExcludeUrlEntity
-import com.intellij.platform.workspace.jps.entities.contentRoot
 import com.intellij.util.execution.ParametersListUtil
 import com.intellij.util.text.nullize
 import works.szabope.plugins.common.run.Exclusions
@@ -30,7 +31,7 @@ fun buildMypyParamList(
     configFilePath.nullize(true)?.let { params.add("--config-file"); params.add(it) }
     arguments.nullize(true)?.let { params.addAll(ParametersListUtil.parse(it)) }
     if (excludeNonProjectFiles) {
-        Exclusions(project).findAll(targets).mapNotNull { getRelativePathFromContentRoot(it)?.toCanonicalPath() }
+        Exclusions(project).findAll(targets).mapNotNull { getRelativePathFromContentRoot(it, project)?.toCanonicalPath() }
             .forEach { params.add("--exclude"); params.add(it) }
     }
     params.addAll(extraArgs)
@@ -39,9 +40,11 @@ fun buildMypyParamList(
 }
 
 // mypy's `--exclude` doesn't work with absolute paths
-private fun getRelativePathFromContentRoot(excludeUrlEntity: ExcludeUrlEntity): Path? {
-    val contentRootPath =
-        excludeUrlEntity.contentRoot?.url?.virtualFile?.path?.let { kotlin.io.path.Path(it) } ?: return null
+private fun getRelativePathFromContentRoot(excludeUrlEntity: ExcludeUrlEntity, project: Project): Path? {
     val exclusionPath = excludeUrlEntity.url.virtualFile?.path?.let { kotlin.io.path.Path(it) } ?: return null
+    val contentRootPath = WorkspaceModel.getInstance(project).currentSnapshot
+        .entities(ContentRootEntity::class.java)
+        .firstOrNull { contentRoot -> contentRoot.excludedUrls.contains(excludeUrlEntity) }
+        ?.url?.virtualFile?.path?.let { kotlin.io.path.Path(it) } ?: return null
     return contentRootPath.relativize(exclusionPath)
 }
