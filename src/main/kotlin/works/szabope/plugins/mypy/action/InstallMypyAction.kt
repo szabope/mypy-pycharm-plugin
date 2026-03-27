e056380b2408107b331c427445ec56eba44ccf8d
Peter Szabo <szabope@users.noreply.github.com>
2026-03-27 11:40:34 +0100

    refactor install tool action

--- src/main/kotlin/works/szabope/plugins/mypy/action/InstallMypyAction.kt
+++ src/main/kotlin/works/szabope/plugins/mypy/action/InstallMypyAction.kt
@@ -2,8 +2,6 @@ package works.szabope.plugins.mypy.action
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.ui.MessageType
-import com.intellij.openapi.wm.ToolWindowManager
 import works.szabope.plugins.common.action.AbstractInstallToolAction
 import works.szabope.plugins.common.services.PluginPackageManagementException
 import works.szabope.plugins.mypy.MypyBundle
@@ -12,6 +10,7 @@ import works.szabope.plugins.mypy.services.MypyPluginPackageManagementService
 import works.szabope.plugins.mypy.toolWindow.MypyToolWindowPanel
 
 class InstallMypyAction : AbstractInstallToolAction(MypyBundle.message("action.InstallMypyAction.done_html")) {
+    override val toolWindowId = MypyToolWindowPanel.ID
     override fun getPackageManager(project: Project) = MypyPluginPackageManagementService.getInstance(project)
 
     override fun handleFailure(failure: Throwable) {
@@ -25,12 +24,6 @@ class InstallMypyAction : AbstractInstallToolAction(MypyBundle.message("action.I
                 DialogManager.showGeneralErrorDialog(failure)
             }
         }
-    }
-
-    override fun notifyPanel(project: Project, message: String) {
-        ToolWindowManager.getInstance(project).notifyByBalloon(
-            MypyToolWindowPanel.ID, MessageType.INFO, message
-        )
     }
 
     companion object {
