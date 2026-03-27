9ad23c40746a09105dca392d181b87ddc25e08cc
Peter Szabo <szabope@users.noreply.github.com>
2026-03-27 11:01:24 +0100

    rename base class for ToolWindowFactory

--- src/main/kotlin/works/szabope/plugins/mypy/toolWindow/MypyToolWindowFactory.kt
+++ src/main/kotlin/works/szabope/plugins/mypy/toolWindow/MypyToolWindowFactory.kt
@@ -1,9 +1,9 @@
 package works.szabope.plugins.mypy.toolWindow
 
 import com.intellij.openapi.project.Project
-import works.szabope.plugins.common.toolWindow.MyToolWindowFactory
+import works.szabope.plugins.common.toolWindow.AbstractToolWindowFactory
 import works.szabope.plugins.mypy.MypyBundle
 
-class MypyToolWindowFactory : MyToolWindowFactory(MypyBundle.message("mypy.toolwindow.name")) {
+class MypyToolWindowFactory : AbstractToolWindowFactory(MypyBundle.message("mypy.toolwindow.name")) {
     override fun createPanel(project: Project) = MypyToolWindowPanel(project)
 }
